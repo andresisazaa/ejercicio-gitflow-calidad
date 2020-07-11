77c47299ed3b072f01922b9dc4e9abd35b6995e9
Angie Marcela Vargas Pardo <angie.vargas@sofka.com.co>
2020-07-10 19:12:01 -0500

    Se agrego datos de Angie Vargas

--- estudiantes.js
+++ estudiantes.js
@@ -1,14 +1,20 @@
 const LISTA_ESTUDIANTES = [
-    {
-        nombre: 'Andrés Felipe Isaza Arboleda',
-        sede: 'Medellín',
-        github: 'andresisazaa',
-        page: './pages/AndresIsaza.html'
-    },
-    {
-        nombre: 'Sebastian Gallego Pulgarin',
-        sede: 'Medellín',
-        github: 'sebasgp',
-        page: './pages/sebasgp.html'
-    }
+  {
+    nombre: "Andrés Felipe Isaza Arboleda",
+    sede: "Medellín",
+    github: "andresisazaa",
+    page: "./pages/AndresIsaza.html",
+  },
+  {
+    nombre: "Sebastian Gallego Pulgarin",
+    sede: "Medellín",
+    github: "sebasgp",
+    page: "./pages/sebasgp.html",
+  },
+  {
+    nombre: "Angie Marcela Vargas Pardo",
+    sede: "Medellín",
+    github: "angiemvargas",
+    page: "./pages/angiemvargas.html",
+  },
 ];
