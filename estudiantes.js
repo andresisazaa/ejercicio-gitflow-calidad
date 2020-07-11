d61ff2a267d9c7625cbb08b0c9839ef4cfa81363
Camilo <camiloa.mejia@udea.edu.co>
2020-07-10 19:08:29 -0500

    done

--- estudiantes.js
+++ estudiantes.js
@@ -10,5 +10,11 @@ const LISTA_ESTUDIANTES = [
         sede: 'Medellín',
         github: 'sebasgp',
         page: './pages/sebasgp.html'
-    }
+    },
+    {
+        nombre: 'Camilo Andrés Mejía Posada',
+        sede: 'Medellín',
+        github: 'AzuladoToujours',
+        page: './pages/camilomp.html'
+    },
 ];
